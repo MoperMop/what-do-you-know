a453a210c0ed875d4d50cba63c7231e20074949e
Daniel Parker <dantiptoe@gmail.com>
2023-10-31 14:41:23 -0600

    added pre-quiz instructions

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //@ts-check
-import { } from "./quiz.js";
+import Quiz from "./quiz.js";
+
+
+const quiz = /** @type {Quiz} */ (document.querySelector("quiz-"));
+
+const instructions = /** @type {HTMLElement} */ (document.querySelector("#instructions"));
+const begin = /** @type {HTMLButtonElement} */ (document.querySelector("#begin"));
+begin.addEventListener("click", () => {
+  quiz.hidden = false;
+  instructions.hidden = true;
+});
