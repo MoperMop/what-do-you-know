305c8de96e3b4a8c01ef7303099c74d5577063c9
Daniel Parker <dantiptoe@gmail.com>
2023-11-09 12:47:09 -0700

    made more questions

--- script.js
+++ script.js
@@ -20,19 +20,3 @@ quiz.addEventListener("finish", () => {
   quizWrapper.hidden = true;
   end.hidden = false;
 });
-
-
-/** @type {number[]} */
-const xValues = [];
-const TARGETLENGTH = quiz.prompts.length + 1;
-while (xValues.length < TARGETLENGTH) {
-  xValues.push(Math.floor(Math.random() * 10) + 1);
-  if (xValues[xValues.length - 1] === xValues?.[xValues.length - 2]) xValues.pop();
-}
-console.log(xValues);
-
-/** @type {MathMLElement} */ (document.querySelector("#instructions mn")).textContent = xValues[0].toString();
-
-
-const xValue = /** @type {MathMLElement} */ (document.querySelector("#x-value mn"));
-xValue.textContent = xValues[0].toString();
